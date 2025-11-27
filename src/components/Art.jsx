e9fdaa0cebb36e7b2a916211e662ce80361270ef
unknown <istwalbimal@gmail.com>
2025-11-27 12:37:49 +0530

    Navlink art issue solved

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -11,7 +11,7 @@ const Art = () => {
 
         const maskTimeline = gsap.timeline({
             scrollTrigger: {
-                trigger: "#art",
+                trigger: "#work",
                 start,
                 end: "bottom center",
                 scrub: 1.5,
@@ -40,7 +40,7 @@ const Art = () => {
     });
 
     return (
-        <div id="art">
+        <section id="work">
             <div className="container mx-auto h-full pt-20">
                 <h2 className="will-fade">The ART</h2>
 
@@ -86,7 +86,7 @@ const Art = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 export default Art;
